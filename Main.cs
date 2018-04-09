d2917544568da19bae62c9a24e85bd92d2bcee96
Lucas Combs <l-combs@hotmail.com>
2018-04-09 13:49:24 -0400

    Hello, World! code

---
+++ Main.cs
@@ -0,0 +1,11 @@
+﻿class Sorts
+{
+
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello, World!");
+
+        //Hold console open
+        while (true) { };
+    }
+}
